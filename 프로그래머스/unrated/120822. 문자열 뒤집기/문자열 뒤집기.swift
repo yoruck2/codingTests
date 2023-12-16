3707fe77f138ab3e10e0e3c994b1f838d5232733
yoruck2 <92171561+yoruck2@users.noreply.github.com>
2023-12-16 14:33:01 +0900

    Update 문자열 뒤집기.swift

--- 프로그래머스/unrated/120822. 문자열 뒤집기/문자열 뒤집기.swift
+++ 프로그래머스/unrated/120822. 문자열 뒤집기/문자열 뒤집기.swift
@@ -3,3 +3,15 @@ import Foundation
 func solution(_ my_string:String) -> String{
     return String(my_string.reversed())
 }
+
+// String에 popLast() 적용
+// import Foundation
+
+// func solution(_ my_string:String) -> String {
+//     var inputString: String = my_string
+//     var result: String = ""
+//     for i in 0 ... my_string.count - 1 {
+//         result += String(inputString.popLast()!)
+//     }
+//     return result
+// }
