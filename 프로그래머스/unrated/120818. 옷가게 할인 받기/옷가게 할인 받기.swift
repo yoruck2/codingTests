31a5152900d2cf593b114158416b5f62e22b26a4
yoruck2 <92171561+yoruck2@users.noreply.github.com>
2023-12-18 10:27:19 +0900

    [level unrated] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120818. 옷가게 할인 받기/옷가게 할인 받기.swift
@@ -0,0 +1,12 @@
+func solution(_ price:Int) -> Int {
+    
+    if price >= 500000 {
+        return price * 80/100
+    } else if price >= 300000 {
+        return price * 90/100
+    } else if price >= 100000 {
+        return price * 95/100
+    }
+    return price
+}
+
