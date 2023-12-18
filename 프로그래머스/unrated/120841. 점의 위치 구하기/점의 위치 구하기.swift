8dfb7d298e5aa1b3c533187d8a21fec941171483
yoruck2 <92171561+yoruck2@users.noreply.github.com>
2023-12-18 10:44:10 +0900

    Update 점의 위치 구하기.swift

--- 프로그래머스/unrated/120841. 점의 위치 구하기/점의 위치 구하기.swift
+++ 프로그래머스/unrated/120841. 점의 위치 구하기/점의 위치 구하기.swift
@@ -18,3 +18,24 @@ func solution(_ dot:[Int]) -> Int {
     
     
 }
+
+
+// switch문 
+//    switch (dot[0], dot[1]) {
+//    case (0..., 0...): return 1
+//    case (...0, 0...): return 2
+//    case (...0, ...0): return 3
+//    case (0..., ...0): return 4
+//    default: return 0
+//    }
+//}
+//
+
+//삼항 연산다
+//func solution(_ dot:[Int]) -> Int {
+//    if dot[0] > 0 {
+//        return dot[1] > 0 ? 1 : 4
+//    }
+//
+//    return dot[1] > 0 ? 2 : 3
+//}
