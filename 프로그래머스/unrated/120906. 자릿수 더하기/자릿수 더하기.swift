2185279968380ebeb6aa16c1a685210a9e25555d
yoruck2 <92171561+yoruck2@users.noreply.github.com>
2023-12-18 11:26:58 +0900

    Update 자릿수 더하기.swift

--- 프로그래머스/unrated/120906. 자릿수 더하기/자릿수 더하기.swift
+++ 프로그래머스/unrated/120906. 자릿수 더하기/자릿수 더하기.swift
@@ -3,3 +3,16 @@ import Foundation
 func solution(_ n:Int) -> Int {
     return Array(String(n)).map { Int(String($0))! }.reduce(0, +)
 }
+
+
+// 나누기랑 나머지 반복문으로 구하기
+// func solution(_ n: Int) -> Int {
+//     var mutableN = n
+//     var answer = 0
+
+//     while mutableN > 0 {
+//         answer += mutableN % 10
+//         mutableN /= 10
+//     }
+//     return answer
+// }
