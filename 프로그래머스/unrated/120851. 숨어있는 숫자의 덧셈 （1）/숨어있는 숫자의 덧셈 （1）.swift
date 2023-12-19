b18089d1c8a02d921371b10b4aebce011fc50e21
yoruck2 <92171561+yoruck2@users.noreply.github.com>
2023-12-19 23:50:03 +0900

    Update 숨어있는 숫자의 덧셈 （1）.swift

--- 프로그래머스/unrated/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.swift
+++ 프로그래머스/unrated/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.swift
@@ -6,8 +6,9 @@ func solution(_ my_string:String) -> Int {
     for i in Array(my_string.filter { Int(String($0)) != nil }) {
         result += Int(String(i))!
     }
-    
     return result
-    
-    
 }
+
+// func solution(_ my_string:String) -> Int {
+//      return my_string.compactMap{Int(String($0))}.reduce(0,+)
+// }
