c6228dfdc91597fe1cbc1c717f1b78be2cd91e7e
yoruck2 <92171561+yoruck2@users.noreply.github.com>
2023-12-23 16:45:36 +0900

    Update 개미 군단.swift

--- 프로그래머스/unrated/120837. 개미 군단/개미 군단.swift
+++ 프로그래머스/unrated/120837. 개미 군단/개미 군단.swift
@@ -14,3 +14,19 @@ func solution(_ hp:Int) -> Int {
     
     return ants
 }
+
+// for문 활용
+func solution(_ hp:Int) -> Int {
+    var hp = hp
+    var result = 0
+    for i in [5, 3, 1] {
+        result += hp / i
+        hp %= i
+    }
+    return result
+}
+
+//간단 버전
+
+2
+func solution(_ hp: Int) -> Int { hp / 5 + hp % 5 / 3 + hp % 5 % 3 }
