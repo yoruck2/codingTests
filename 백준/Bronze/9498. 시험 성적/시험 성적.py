458abdf8ce867a3846a8680652ad5d9cbdd72eb6
yoruck2 <92171561+yoruck2@users.noreply.github.com>
2024-02-06 21:48:21 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,13 @@
+A = list(map(int, input().split()))
+
+a = A[0]
+if 90 <= a <= 100:
+    print("A")
+elif 80 <= a <= 89:
+    print("B")
+elif 70 <= a <= 79:
+    print("C")
+elif 60 <= a <= 69:
+    print("D")
+else:
+    print("F")
