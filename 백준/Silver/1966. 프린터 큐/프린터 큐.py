21ddd68293f7e766e5f4ff4f3cf6021bac1091db
yoruck2 <92171561+yoruck2@users.noreply.github.com>
2024-04-07 00:00:04 +0900

    [Silver III] Title: 프린터 큐, Time: 92 ms, Memory: 34052 KB -BaekjoonHub

---
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+from collections import deque
+
+TC = int(input())
+
+
+
+for _ in range(TC):
+    N, M = map(int, input().split())
+    docs = deque(map(int, input().split()))
+    docs = deque([(d, i) for i, d in enumerate(docs)])
+    count = 0
+
+    while True:
+        if docs[0][0] == max(docs, key=lambda x:x[0])[0]:
+            count += 1
+            if docs[0][1] == M:
+                print(count)
+                break
+            else:
+                docs.popleft()
+        else:
+            docs.append(docs.popleft())
